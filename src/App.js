6f9863e1b7d302d901e6017c47833a6442fa35f2
dannielgloria <danniel.gloria@gmail.com>
2022-11-02 20:02:11 -0600

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola este es un ejemplo de como subir su app REACT a NETLIFY
         </p>
         <a
           className="App-link"
